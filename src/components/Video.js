a35e3e0b502a36a25c6afd2bd3576d1ec17cc4ee
Uday Tyagi <146240633+20-uday-06@users.noreply.github.com>
2024-08-10 23:47:48 +0530

    final ho gaya shivansh bhaio

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,35 +1,23 @@
-<<<<<<< HEAD
 import React from 'react';
 import './Video.css';
 import Videoo from './video1.mp4';
 
 function Video() {
     return (
-        <div id="main" style={{ position: "absolute", backdropFilter:"blur(6px)", zIndex: "15" }}>
-                            `<video autoPlay loop style={{  height: "90%", borderRadius
-                                :"20px" , position:"relative" , left:"34%" , top:"30px"
-                             }}>
-                    <source sr`c={Videoo} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+        <div id="main" style={{ position: "absolute", backdropFilter: "blur(6px)", zIndex: "15" }}>
+            `<video autoPlay loop style={{
+                height: "90%", borderRadius
+                    : "20px", position: "relative", left: "34%", top: "30px"
+            }}>
+                <source src={Videoo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div style={{ position: "absolute", top: "90%", left: "42%" }}>
-=======
-import React from 'react'
-import './Video.css'
-
-function Video() {
-    return (
-        <div id="main" style={{position: "absolute", backgroundColor: "#fff", zIndex: "15"}}>
-            <div id = "heart" style={{position: "absolute", left: "30%"}}>
-                
-            </div>
-            <div style={{position: "absolute", top: "90%", left: "42%"}}>
->>>>>>> parent of 7fae931 (shivansh madarchod ke website khatam)
                 <a href="/swipper">
-                <button class="button" style={{fontSize: "2vw", borderRadius: "20px", padding: "0.7vw 2.3vw", color: "white", backgroundColor: "red", fontWeight: "bold" }}>Continue</button>
-                </a>
-            </div>
-    </div>
+                <button class="button" style={{ fontSize: "2vw", borderRadius: "20px", padding: "0.7vw 2.3vw", color: "white", backgroundColor: "red", fontWeight: "bold" }}>Continue</button>
+            </a>
+        </div>
+    </div >
     )
 }
 
