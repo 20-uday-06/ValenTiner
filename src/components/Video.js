9fb49601a352518ee7061738938f947525a786e2
Uday Tyagi <146240633+20-uday-06@users.noreply.github.com>
2024-08-10 23:45:15 +0530

    finalllyyyyyyyyyyyyy

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
+import React from 'react';
+import './Video.css';
+import Videoo from './video1.mp4';
+
+function Video() {
+    return (
+        <div id="main" style={{ position: "absolute", backdropFilter:"blur(6px)", zIndex: "15" }}>
+                            `<video autoPlay loop style={{  height: "90%", borderRadius
+                                :"20px" , position:"relative" , left:"34%" , top:"30px"
+                             }}>
+                    <source sr`c={Videoo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            <div style={{ position: "absolute", top: "90%", left: "42%" }}>
+=======
 import React from 'react'
 import './Video.css'
 
@@ -8,6 +24,7 @@ function Video() {
                 
             </div>
             <div style={{position: "absolute", top: "90%", left: "42%"}}>
+>>>>>>> parent of 7fae931 (shivansh madarchod ke website khatam)
                 <a href="/swipper">
                 <button class="button" style={{fontSize: "2vw", borderRadius: "20px", padding: "0.7vw 2.3vw", color: "white", backgroundColor: "red", fontWeight: "bold" }}>Continue</button>
                 </a>
